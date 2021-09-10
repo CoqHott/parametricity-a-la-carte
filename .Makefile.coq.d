5ec3dcc82927841763ba3526d6e330f0f489a629
Thomas Lamiaux <thomas.lamiaux@gmail.com>
2021-09-11 00:38:24 +0200

    Renaming + seperating some files
    -> The files are now indexed by V1,V2,V3 in order to know which is the current version.
    -> The V3 file is now seperated in 3 files in order to better distinguish the working parts.

--- .Makefile.coq.d
+++ .Makefile.coq.d
@@ -7,3 +7,9 @@ theories/HoTT_axioms.vos theories/HoTT_axioms.vok theories/HoTT_axioms.required_
 theories/Equiv_def.vo theories/Equiv_def.glob theories/Equiv_def.v.beautified theories/Equiv_def.required_vo: theories/Equiv_def.v theories/HoTT.vo theories/HoTT_axioms.vo
 theories/Equiv_def.vio: theories/Equiv_def.v theories/HoTT.vio theories/HoTT_axioms.vio
 theories/Equiv_def.vos theories/Equiv_def.vok theories/Equiv_def.required_vos: theories/Equiv_def.v theories/HoTT.vos theories/HoTT_axioms.vos
+theories/V3_Param_CComega_fixpoint.vo theories/V3_Param_CComega_fixpoint.glob theories/V3_Param_CComega_fixpoint.v.beautified theories/V3_Param_CComega_fixpoint.required_vo: theories/V3_Param_CComega_fixpoint.v theories/HoTT.vo theories/HoTT_axioms.vo theories/Equiv_def.vo
+theories/V3_Param_CComega_fixpoint.vio: theories/V3_Param_CComega_fixpoint.v theories/HoTT.vio theories/HoTT_axioms.vio theories/Equiv_def.vio
+theories/V3_Param_CComega_fixpoint.vos theories/V3_Param_CComega_fixpoint.vok theories/V3_Param_CComega_fixpoint.required_vos: theories/V3_Param_CComega_fixpoint.v theories/HoTT.vos theories/HoTT_axioms.vos theories/Equiv_def.vos
+theories/V3_Param_ind_fixpoint.vo theories/V3_Param_ind_fixpoint.glob theories/V3_Param_ind_fixpoint.v.beautified theories/V3_Param_ind_fixpoint.required_vo: theories/V3_Param_ind_fixpoint.v theories/HoTT.vo theories/HoTT_axioms.vo theories/Equiv_def.vo theories/V3_Param_CComega_fixpoint.vo
+theories/V3_Param_ind_fixpoint.vio: theories/V3_Param_ind_fixpoint.v theories/HoTT.vio theories/HoTT_axioms.vio theories/Equiv_def.vio theories/V3_Param_CComega_fixpoint.vio
+theories/V3_Param_ind_fixpoint.vos theories/V3_Param_ind_fixpoint.vok theories/V3_Param_ind_fixpoint.required_vos: theories/V3_Param_ind_fixpoint.v theories/HoTT.vos theories/HoTT_axioms.vos theories/Equiv_def.vos theories/V3_Param_CComega_fixpoint.vos
